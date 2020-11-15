31e52e504d3ec642cc69e595ae08f6083b5fea6d
jamesskywalker <jamescarr28@hotmail.co.uk>
2020-11-15 15:02:58 +0000

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+// webpack.config.js
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const webpack = require('webpack');
+
+module.exports = env => {
+    const DE = require('dotenv').config({path: (env.NODE_ENV == 'production' ? './.env' : './.env.local')}).parsed;
+    return {
+        entry: {
+            app_private: './app/js/app.js'
+        },
+        mode: env.NODE_ENV,
+        output: {
+            path: __dirname + '/js/',
+            filename: '[name].js'
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.vue$/,
+                    loader: 'vue-loader'
+                },
+                // this will apply to both plain `.js` files
+                // AND `<script>` blocks in `.vue` files
+                {
+                    test: /\.js$/,
+                    loader: 'babel-loader'
+                },
+                // this will apply to both plain `.css` files
+                // AND `<style>` blocks in `.vue` files
+                {
+                    test: /\.css$/,
+                    use: [
+                        'vue-style-loader',
+                        'css-loader'
+                    ]
+                }
+            ]
+        },
+        resolve: {
+            alias: {
+                vue: env.NODE_ENV == 'production' ? 'vue/dist/vue.min.js' : 'vue/dist/vue.js'
+            }
+        },
+        plugins: [
+            // make sure to include the plugin for the magic
+            new VueLoaderPlugin(),
+            //app_env_obj
+            new webpack.DefinePlugin({
+                'env': JSON.stringify(DE)
+            })
+
+
+        ]
+    }
+
+};
