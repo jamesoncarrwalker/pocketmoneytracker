18f6bf7c890f38cf72e03b41b989000bc415033d
jamesskywalker <jamescarr28@hotmail.co.uk>
2020-11-23 20:07:51 +0000

    dont need this now

--- app/vue/shared/components/MainApp.vue
+++ app/vue/shared/components/MainApp.vue
@@ -2,14 +2,6 @@
     <div class="container-fluid">
 
         <div class="row">
-            <div class="container">
-                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                    <h5>THIS IS THE HEADER</h5>
-                </div>
-            </div>
-        </div>
-
-        <div class="row">
 
             <div class="container router-view-container">
 
@@ -17,14 +9,6 @@
 
             </div>
 
-        </div>
-
-        <div class="row">
-            <div class="container">
-                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                    <h5>THIS IS THE FOOTER</h5>
-                </div>
-            </div>
         </div>
 
     </div>
